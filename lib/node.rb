683423d41b879c3d3a299909d080920e6d5b0a0b
Luis Garcia <garcia@yahoo.com>
2017-11-30 16:01:51 -0700

    Created test and functionality for surname and next_node

--- lib/node.rb
+++ lib/node.rb
@@ -1,7 +1,10 @@
 class Node
+  attr_accessor :surname, :next_node
 
-  def initialize
+  def initialize(surname, next_node = nil)
     @head = nil  
+    @surname = surname
+    @next_node = nil
   end
 
 end
--- test/node_test.rb
+++ test/node_test.rb
@@ -4,14 +4,22 @@ require './lib/node'
 
 class NodeTest < Minitest::Test
 
-attr_reader :node
+  attr_reader :node
 
   def setup
-    @node = Node.new
+    @node = Node.new("Burke")
   end
 
   def test_instance_of_linked_list
     assert_instance_of Node, node    
+  end
+
+  def test_node_has_a_surname
+    assert_equal "Burke", node.surname
+  end
+
+  def test_next_node_returns_nil
+    assert_nil node.next_node
   end
 
 end
